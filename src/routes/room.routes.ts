543f143fca36bb7c25e021bb1c356f3a3de1f8fb
Estiknok <95084542+Estiknok@users.noreply.github.com>
2022-10-01 11:06:04 +0200

    Fixed some minor things

--- src/routes/room.routes.ts
+++ src/routes/room.routes.ts
@@ -9,7 +9,7 @@ const router = Router();
  * !The routes are already prefixed with /room
  * !Do not create the functions here, create them in the controller file
  */
-router.get('/', verifyToken, roomController.getRoom);
+router.post('/', verifyToken, roomController.getRoom);
 
 router.delete('/', verifyToken, roomController.deleteRoom);
 
