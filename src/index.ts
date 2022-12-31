6e7df03dbdd20129061814cce2eb8cd402c7fb0c
Estiknok <95084542+Estiknok@users.noreply.github.com>
2022-12-31 12:47:05 +0100

    fix some minor problems

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { Server, Socket } from "socket.io";
 import Message from "./models/Message";
 import User from "./models/User";
 import {createMessage, getMessages} from './utils/message.util';
+import {IUserResponse} from './interfaces/IClientResponse';
 
 const app: App = new App();
 
@@ -21,7 +22,25 @@ db.connectDB();
 //io
 const io = new Server(5050, { cors: { origin: "*" } });
 
+const connectedUsers: Map<string, string> = new Map();
+
 io.on("connection", (socket: Socket) => {
+    socket.on("add-user", async (user) => {
+        const newUser = await User.findOne({username: user});
+
+        const allMessages: string[] = (await getMessages()).map(each => each.message);
+        socket.emit("all-msg", (allMessages).toString());
+        
+        if(connectedUsers.get(user)) return;
+        if(!newUser) return;
+
+        connectedUsers.set(user, socket.id);
+
+        const newUserResponse: IUserResponse = {avatarUrl: newUser.avatarImage.avatarImageUrl, email: newUser.email, username: newUser.username};
+        socket.broadcast.emit("new-usr", newUserResponse);
+        console.log(user);
+    });
+
     socket.on("send-msg", async (msg) => {
         const foundUser = await User.findOne({ username: msg.person });
         
