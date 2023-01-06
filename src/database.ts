d7c14df932826fdb2b404d615f6dda8fd8a33dd1
Estiknok <95084542+Estiknok@users.noreply.github.com>
2023-01-06 22:39:35 +0100

    trying db

--- src/database.ts
+++ src/database.ts
@@ -5,12 +5,14 @@ export async function connectDB() {
     try{
         //TODO: Add to the config the user and password for the mongo db
         const options: ConnectOptions = {
-            user: config.MONGO_USER,
-            pass: config.MONGO_PASSWORD,
+            auth: {
+                username: config.MONGO_USER,
+                password: config.MONGO_PASSWORD
+            }
         }
 
         mongoose.set("strictQuery", false);
-        const db = await mongoose.connect(`mongodb://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_HOST}:${config.MONGO_PORT}/${config.MONGO_DATABASE}`);
+        const db = await mongoose.connect(`mongodb://${config.MONGO_HOST}:${config.MONGO_PORT}/${config.MONGO_DATABASE}`, options);
         console.log(`Database connected: ${db.connection.name}`);
     }
     catch(err){
