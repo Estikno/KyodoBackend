6b836c9da3211d506ee1c79d0ae4e74932286e47
Estiknok <95084542+Estiknok@users.noreply.github.com>
2023-01-06 22:44:34 +0100

    3rd try

--- src/database.ts
+++ src/database.ts
@@ -12,7 +12,7 @@ export async function connectDB() {
         }
 
         mongoose.set("strictQuery", false);
-        const db = await mongoose.connect(`mongodb://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_HOST}:${config.MONGO_PORT}`);
+        const db = await mongoose.connect(`mongodb://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_HOST}:${config.MONGO_PORT}/${config.MONGO_DATABASE}`);
         console.log(`Database connected: ${db.connection.name}`);
     }
     catch(err){
